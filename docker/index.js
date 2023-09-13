caf09ce7b41aceee7668619ec742743380bfade7
Vladimir Barbaros <vladimir.b@bannernow.com>
2023-09-13 10:44:52 +0300

    docker: a rudimentary service in a docker image

---
+++ docker/index.js
@@ -0,0 +1,67 @@
+const cli = require('@vbarbarosh/node-helpers/src/cli');
+const express = require('express');
+const express_routes = require('@vbarbarosh/express-helpers/src/express_routes');
+const express_run = require('@vbarbarosh/express-helpers/src/express_run');
+const ffmpeg_trim_crop_resize = require('../src/ffmpeg_trim_crop_resize');
+const ffprobe = require('../src/ffprobe');
+const fs_path_resolve = require('@vbarbarosh/node-helpers/src/fs_path_resolve');
+const fs_tempdir = require('@vbarbarosh/node-helpers/src/fs_tempdir');
+const path = require('path');
+const shell = require('@vbarbarosh/node-helpers/src/shell');
+const shell_json = require('@vbarbarosh/node-helpers/src/shell_json');
+
+cli(main);
+
+async function main()
+{
+    const app = express();
+
+    app.use(express.static(path.resolve(__dirname, 'static')));
+
+    express_routes(app, [
+        {req: 'GET /', fn: help},
+        {req: 'POST /', fn: ffmpeg},
+        {req: 'ALL *', fn: page404},
+    ]);
+
+    await express_run(app, 3000, '0.0.0.0');
+}
+
+async function page404(req, res)
+{
+    res.status(404).send(`Page not found: ${req.path}`);
+}
+
+async function help(req, res)
+{
+    res.type('text').send(`usage:
+POST / {trim: [{start: 0, end: 5}, {start: 8: end: 9}], crop: {x: 0, y: 0, w: 100, h: 100}, resize: {w: 50, h: 50}}
+`);
+}
+
+async function ffmpeg(req, res)
+{
+    await fs_tempdir(async function (d) {
+        // https://test-videos.co.uk/bigbuckbunny/mp4-h264
+        const input_url = 'https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_30MB.mp4';
+        const output = fs_path_resolve(d, 'a.mp4');
+        const input = fs_path_resolve(d, 'input');
+        log(`Downloading ${input_url}...`);
+        await shell(['curl', '-sfS', input_url, '-o', input]);
+        log(`Reading metadata...`);
+        const probe = await shell_json(ffprobe({input}));
+        const trim = null;//[];
+        const crop = null;//{};
+        const resize = {w: 400, h: 300};
+        log(`Rendering mp4...`);
+        await shell(ffmpeg_trim_crop_resize({probe, input, output, trim, crop, resize}));
+        log('Sending mp4 back...');
+        res.sendFile(output);
+        log('Done');
+    });
+}
+
+function log(s)
+{
+    console.log(s);
+}

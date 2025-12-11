753714a38c80c37f3642ca28d50eb85039982145
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2025-12-11 18:30:58 +0200

    format_ffmpeg_progress: expected_duration_us: ffmpeg prints only microseconds

--- src/format_ffmpeg_progress.js
+++ src/format_ffmpeg_progress.js
@@ -4,14 +4,17 @@ function format_ffmpeg_progress(ffmpeg_progress)
         return '~';
     }
 
-    const {fps, out_time, out_time_ms, speed, expected_duration_ms} = ffmpeg_progress;
+    // All time fields in `-progress` are in microseconds, regardless of name.
+    // The name out_time_ms is therefore misleading, but preserved for compatibility. 🤯
+
+    const {fps, out_time, out_time_ms, speed, expected_duration_us} = ffmpeg_progress;
     const fps_or_speed = fps ? Math.max(1, Math.round(fps)) + 'fps' : (speed ?? '~');
 
-    if (expected_duration_ms) {
-        return `${(out_time_ms/expected_duration_ms*100).toFixed(2)}% ${fps_or_speed}`;
+    if (expected_duration_us && out_time_ms) {
+        return `${(out_time_ms/expected_duration_us*100).toFixed(2)}% at ${fps_or_speed}`;
     }
 
-    return `${(out_time||'').replace(/\..+$/, '') || '~'} ${fps_or_speed}`.replace(/~\s*~/g, '~');
+    return `${(out_time||'').replace(/\..+$/, '') || '~'} at ${fps_or_speed}`.replace('~ at ~', '~');
 }
 
 module.exports = format_ffmpeg_progress;
--- src/format_ffmpeg_progress.test.js
+++ src/format_ffmpeg_progress.test.js
@@ -10,10 +10,24 @@ describe('format_ffmpeg_progress', function () {
         const expected = await fs_read_json(fs_path_resolve(__dirname, 'progress.d/audio-rpad/format_ffmpeg_progress.json'));
         assert.deepStrictEqual(actual, expected);
     });
+    it('audio-rpad2', async function () {
+        const items = await fs_read_json(fs_path_resolve(__dirname, 'progress.d/audio-rpad/stream_ffmpeg_progress.json'));
+        items.forEach(v => v.expected_duration_us = 605877551);
+        const actual = items.map(format_ffmpeg_progress);
+        const expected = await fs_read_json(fs_path_resolve(__dirname, 'progress.d/audio-rpad/format_ffmpeg_progress2.json'));
+        assert.deepStrictEqual(actual, expected);
+    });
     it('video-shortest', async function () {
         const items = await fs_read_json(fs_path_resolve(__dirname, 'progress.d/video-shortest/stream_ffmpeg_progress.json'));
         const actual = items.map(format_ffmpeg_progress);
         const expected = await fs_read_json(fs_path_resolve(__dirname, 'progress.d/video-shortest/format_ffmpeg_progress.json'));
+        assert.deepStrictEqual(actual, expected);
+    });
+    it('video-shortest2', async function () {
+        const items = await fs_read_json(fs_path_resolve(__dirname, 'progress.d/video-shortest/stream_ffmpeg_progress.json'));
+        items.forEach(v => v.expected_duration_us = 5.88*1000000);
+        const actual = items.map(format_ffmpeg_progress);
+        const expected = await fs_read_json(fs_path_resolve(__dirname, 'progress.d/video-shortest/format_ffmpeg_progress2.json'));
         assert.deepStrictEqual(actual, expected);
     });
 });

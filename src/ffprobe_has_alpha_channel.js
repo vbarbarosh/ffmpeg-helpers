e197eb13866b07dcdd5dca78df46dfbe197fa744
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2025-12-16 09:55:51 +0200

    update ffprobe_has_alpha_channel

--- src/ffprobe_has_alpha_channel.js
+++ src/ffprobe_has_alpha_channel.js
@@ -2,8 +2,7 @@ function ffprobe_has_alpha_channel(ffprobe)
 {
     return Boolean(ffprobe.streams.find(function (v) {
         return (v.codec_name === 'vp8' || v.codec_name === 'vp9')
-            && (v.pix_fmt === 'yuv420p')
-            && (v.tags.alpha_mode || v.tags.ALPHA_MODE);
+            && (v.tags?.alpha_mode === '1' || v.tags?.ALPHA_MODE === '1');
     }));
 }
 
